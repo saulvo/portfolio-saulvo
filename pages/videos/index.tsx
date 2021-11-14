a90369a770a334a92193371276ef41732b8c4cc5
sonvt-fe <vtson23597@gmail.com>
2021-11-14 19:46:15 +0700

    ssg videos

--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -1,21 +1,22 @@
 import * as api from '@/api/youtube';
 import Layout from '@/components/common/Layout';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { GetStaticProps, InferGetServerSidePropsType } from 'next';
 import dynamic from 'next/dynamic';
 const VideosPage = dynamic(() => import('@/components/videos'));
 
-export default function Videos(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Videos(props: InferGetServerSidePropsType<typeof getStaticProps>) {
   return <VideosPage {...props} />;
 }
 
 Videos.Layout = Layout;
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+export async function getStaticProps(ctx: GetStaticProps) {
   const videoList = await api.getPlaylistById('PLw46C1_ab136VIukkG2lGQK2jlakNVUsO');
 
   return {
     props: {
       videoList,
+      revalidate: 600,
     },
   };
 }
